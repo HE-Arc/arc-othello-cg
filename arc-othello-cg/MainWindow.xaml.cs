63a27b983aa3016aef1e5de817086c63d71bb042
Noah Graells <noah.graells@he-arc.ch>
2019-01-10 22:18:48 +0100

    Basic board design creation

--- arc-othello-cg/MainWindow.xaml.cs
+++ arc-othello-cg/MainWindow.xaml.cs
@@ -20,12 +20,35 @@ namespace arc_othello_cg
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() : this(9, 7)
+        {
+
+        }
+
+        public MainWindow(int width, int height)
         {
             InitializeComponent();
 
             GameManager gameManager = new GameManager();
             gameManager.Run();
+
+            Box.Stretch = Stretch.Uniform;
+
+            PlayGrid.Columns = width;
+            PlayGrid.Rows = height;
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    Image btn = new Image();
+                    btn.Source = new BitmapImage(new Uri(@"/Resources/two.png", UriKind.Relative));
+
+                    PlayGrid.Children.Add(btn);
+                    Grid.SetColumn(btn, col);
+                    Grid.SetRow(btn, row);
+                }
+            }
         }
     }
 }
